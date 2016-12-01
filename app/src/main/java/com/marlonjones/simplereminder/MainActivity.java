5b47d36d9501757a151f39370d24d1a5a8ad0555
Marlon Jones <marlonjones234@gmail.com>
2016-12-01 14:36:47 -0600

    Icon

--- app/src/main/java/com/marlonjones/simplereminder/MainActivity.java
+++ app/src/main/java/com/marlonjones/simplereminder/MainActivity.java
@@ -1,10 +1,16 @@
 package com.marlonjones.simplereminder;
 
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
 import android.view.View;
@@ -15,6 +21,8 @@ import android.widget.Toast;
 import com.afollestad.materialdialogs.MaterialDialog;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int NOTIFICATION_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +37,15 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onInput(MaterialDialog dialog, CharSequence input) {
                         //notification
-                        NotificationManager notificationManager = (NotificationManager)
-                                getSystemService(NOTIFICATION_SERVICE);
+                            NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
+                            builder.setOngoing(true);
+                            builder.setSmallIcon(R.drawable.ic_stat_notification);
+                            builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+                            builder.setContentTitle("Notifications Title");
+                            builder.setContentText("Your notification content here.");
+                            builder.setSubText("Tap to view documentation about notifications.");
+                            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                            notificationManager.notify(NOTIFICATION_ID, builder.build());
                         //toast
                         Toast.makeText(MainActivity.this, "Reminder Created. Set as Ongoing Notification",
                                 Toast.LENGTH_SHORT).show();

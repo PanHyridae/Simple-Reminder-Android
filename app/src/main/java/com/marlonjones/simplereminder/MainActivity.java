d3e79b215a6f4e4f045fae02e67bfbe2f6f1a1a8
Marlon Jones <marlonjones234@gmail.com>
2016-12-03 23:30:27 -0600

    Removed things, fixed things - works mostly

--- app/src/main/java/com/marlonjones/simplereminder/MainActivity.java
+++ app/src/main/java/com/marlonjones/simplereminder/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
 import android.text.InputType;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         //Aidan's Library - Material Dialogs - Extends to .show(); and wraps around the Notification and other parts
         //Input Dialog
         new MaterialDialog.Builder(this)
@@ -37,20 +39,21 @@ public class MainActivity extends AppCompatActivity {
                         //notification body
                         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                             NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
-                        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0,
+                            PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 1,
                                 new Intent(getApplicationContext(), MainActivity.class)
                                         .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP),
                                 0);
-
-                            //Rest of Notification
+                            Intent close = new Intent (getApplicationContext(), MainActivity.class);
                             builder.setStyle(new NotificationCompat.BigTextStyle().bigText(input.toString())); //BigText
                             builder.setOngoing(true); //Make persistent
                             builder.setContentIntent(pendingIntent); //OnClick for Reopening App
+                            //builder.setContentIntent(closeIntent); //Removed until Working
                             builder.setSmallIcon(R.drawable.ic_note);
                             builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
                             builder.setContentTitle("Remember!");
                             builder.setContentText(input.toString()); //Get text from dialog input
-                            builder.addAction(R.drawable.ic_action_name, "Done", closeIntent); //Action for the closer
+                            //PendingIntent closeIntent = PendingIntent.getActivities(getApplicationContext(),0, new Intent[]{close},PendingIntent.FLAG_UPDATE_CURRENT);                          builder.setContentIntent(closeIntent);
+                            builder.addAction(R.drawable.ic_action_name, "Done", pendingIntent/*closeIntent*/); //Action for the closer
                             notificationManager.notify(NOTIFICATION_ID, builder.build());
 
                         //toast

2cfb8ff31e4d122408079b62a139860c8c1b34ae
Marlon Jones <marlonjones234@gmail.com>
2016-12-13 00:38:52 -0600

    Testing

--- app/src/main/java/com/marlonjones/simplereminder/ConfirmActivity.java
+++ app/src/main/java/com/marlonjones/simplereminder/ConfirmActivity.java
@@ -8,12 +8,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
 import android.text.InputType;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
+
+import static java.security.AccessController.getContext;
 
 public class ConfirmActivity extends AppCompatActivity {
     //TODO - ADD ANDROID WEAR AND TIME FUNCTIONALITY
@@ -27,6 +31,29 @@ public class ConfirmActivity extends AppCompatActivity {
                 .content(R.string.confirm_content)
                 .positiveText(R.string.confirm_pos)
                 .negativeText(R.string.confirm_neg)
-                .show();
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        Intent reOpen = new Intent (getApplicationContext(), MainActivity.class);
+                        getApplicationContext().startActivity(reOpen);
+                    }
+                })
+                .onNegative(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                        notificationManager.cancel(NOTIFICATION_ID);
+                    }
+                })
+                .dismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                finish();
+            }
+        }).show();
+    }
+    //Closes the app when the back button is pressed
+    public void onBackPressed(){
+        finish();
     }
 }

6c72f90700703f2d5b23723bea0bfe5ad2fe1d30
Marlon Jones <marlonjones234@gmail.com>
2016-12-12 23:26:15 -0600

    New Java Class

---
+++ app/src/main/java/com/marlonjones/simplereminder/ConfirmActivity.java
@@ -0,0 +1,27 @@
+package com.marlonjones.simplereminder;
+/**
+ * Java Class made by Marlon Jones (MJonesDev) on 12/12/2016.
+ */
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+import android.text.InputType;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+
+public class ConfirmActivity extends AppCompatActivity {
+    //TODO - ADD ANDROID WEAR AND TIME FUNCTIONALITY
+    public static final int NOTIFICATION_ID = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}

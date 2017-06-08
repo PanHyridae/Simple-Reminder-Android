f18afba3c49d266d5eaa04b0c83636d73a472818
Marlon Jones <marlonjones234@gmail.com>
2017-06-08 16:16:46 -0500

    Starting on Timer Activities

---
+++ app/src/main/java/com/marlonjones/simplereminder/TimeBroadcast.java
@@ -0,0 +1,11 @@
+package com.marlonjones.simplereminder;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Marlon Jones (@MJonesDev on 6/8/2017.
+ * github.com/MJonesDev
+ */
+
+public class TimeBroadcast extends AppCompatActivity {
+}
